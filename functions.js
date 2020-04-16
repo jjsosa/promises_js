b83e6b94031082f7bdabb01836b47dddc84cfc66
Juan José Sosa Osuna <sosaosuna@gmail.com>
2020-04-16 17:46:17 +0200

    added file with functions

---
+++ functions.js
@@ -0,0 +1,26 @@
+function gotData(data) {
+    console.log('DATA -> ', data);
+}
+
+function gotError(error) {
+    console.log('ERROR -> ', error);
+}
+
+function functionReturnPromise(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getNombre(username, apiUrl) {
+    const url = apiUrl + username;
+    fetch(url)
+        .then( res => res.json() )
+        .then( json => { console.log(json.name)  })
+        // .error ( err => { console.log('error: ', err) })
+}
+
+module.exports = {
+    gotData:gotData,
+    gotError:gotError,
+    functionReturnPromise:functionReturnPromise,
+    getNombre:getNombre
+}
