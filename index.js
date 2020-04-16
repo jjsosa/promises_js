f19555a30f92debaa3ce2b35628b9be426273ed4
Juan José Sosa Osuna <sosaosuna@gmail.com>
2020-04-16 17:47:20 +0200

    node-fetch dependency added in functions file

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-let apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+const myFunctions = require('./functions');
 
-let promise = fetch(apiUrl);
+let apiUrl = 'https://api.github.com/users/';
 
-promise.then(gotData);
-promise.catch(gotError);
-
-function gotData(data) {
-    console.log('DATA -> ', data);
-}
-
-function gotError(error) {
-    console.log('ERROR -> ', error);
-}
+myFunctions.getNombre('jjsosa', apiUrl);
