0907537baa5e06f79a644183d66c4e47675cb7ae
Juan José Sosa Osuna <sosaosuna@gmail.com>
2020-04-16 17:53:03 +0200

    Executing functions on index.js

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const myFunctions = require('./functions');
 
 let apiUrl = 'https://api.github.com/users/';
 
-myFunctions.getNombre('jjsosa', apiUrl);
+// Ejecución y control de la promesa en el archivo de funciones
+myFunctions.getNombreFromUsername('jjsosa', apiUrl);
+
+// Ejecución y control de la promesa en este archivo
+myFunctions.getNombreFromUsernameReturnPromise('jjsosa', apiUrl)
+    .then( res => res.json() )
+    .then( json => { console.log(json.name) })
+    .catch(error => console.log(error));
